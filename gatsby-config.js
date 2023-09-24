0b4c535da1dc60d91946dc7705116222838b5d2f
Vasco Feeney <vasco.feeney@gmail.com>
2023-09-24 19:42:12 +0100

    fixed gatsby manifest config

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,11 +40,16 @@ module.exports = {
         theme_color: "#6b37bf",
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: "standalone"
+        display: "standalone",
+        icon: "static/favicon.ico", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
       },
     },
-    "gatsby-plugin-offline"
     
+    "gatsby-plugin-offline"
   ],
 }
 
