6f72aed160778e12eb14b9129e443afe350683dc
Vasco Feeney <vasco.feeney@gmail.com>
2023-09-24 19:38:41 +0100

    making it a progressive app

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,21 @@ module.exports = {
         extensions: [`.md`, `.mdx`],
       },
     },
-    `gatsby-plugin-react-helmet`
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "GatsbyJS Bootstrap tutorial",
+        short_name: "GatsbyJS",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone"
+      },
+    },
+    "gatsby-plugin-offline"
     
   ],
 }
